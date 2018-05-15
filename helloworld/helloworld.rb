96ddc3bd4b20f5fdcc0c2436c0231512461d6e3d
Ryan Prior <ryanprior@gmail.com>
2018-05-15 15:48:03 -0400

    Updates demo in preparation for enhanced operation mode
    Goals:
    * allow on-demand rotation of the shared secret with no downtime
    * allow creation of a swarm of consumers, rather than just one

--- helloworld/helloworld.rb
+++ helloworld/helloworld.rb
@@ -3,30 +3,46 @@ require 'rack/token_auth'
 require 'securerandom'
 require 'conjur-api'
 
-# Get service configuration inputs from environment
-conjur_account = ENV['CONJUR_ACCOUNT']
-conjur_username = ENV['CONJUR_AUTHN_LOGIN']
-conjur_api_key = ENV['CONJUR_AUTHN_API_KEY']
-conjur_cert = ENV['CONJUR_CERT_FILE']
-secret_id = ENV['DEMO_SERVICE_KEY_ID']
-resource_id = "#{conjur_account}:variable:#{secret_id}"
+class HelloWorld < Sinatra::Base
+  def initialize
+    # Get service configuration inputs from environment
+    conjur_account = ENV['CONJUR_ACCOUNT']
+    conjur_username = ENV['CONJUR_AUTHN_LOGIN']
+    conjur_api_key = ENV['CONJUR_AUTHN_API_KEY']
+    conjur_cert = ENV['CONJUR_CERT_FILE']
+    secret_id = ENV['DEMO_SERVICE_KEY_ID']
+    @@resource_id = "#{conjur_account}:variable:#{secret_id}"
 
-OpenSSL::SSL::SSLContext::DEFAULT_CERT_STORE.add_file conjur_cert if conjur_cert
+    OpenSSL::SSL::SSLContext::DEFAULT_CERT_STORE.add_file conjur_cert if conjur_cert
+    @@conjur_api = Conjur::API.new_from_key conjur_username, conjur_api_key
+    rotate_shared_secret
+  end
 
-# Generate service key value and store it in the attached
-# Conjur service
-secret = SecureRandom.hex(20)
-api = Conjur::API.new_from_key conjur_username, conjur_api_key
-api.resource(resource_id).add_value secret
+  helpers do
+    def rotate_shared_secret
+      # Generate service key value and store it in the attached
+      # Conjur service
+      @@secret = SecureRandom.hex(20)
+      @@conjur_api.resource(@@resource_id).add_value @@secret
+    end
+  end
 
-# Set up token authentication for our service with
-# the generated service key
-use Rack::TokenAuth do |token, _options, _env|
-  token == secret
-end
+  # Set up token authentication for our service with
+  # the generated service key
+  use Rack::TokenAuth do |token, _options, _env|
+    token == @@secret
+  end
 
-# Our service simply responds to an authenticated GET
-# request on the root path with the string, "Hello world!"
-get '/' do
-  'Hello world!'
+  # Our service simply responds to an authenticated GET
+  # request on the root path with the string, "Hello world!"
+  get '/' do
+    'Hello world!'
+  end
+
+  get '/rotate' do
+    rotate_shared_secret
+    halt 204
+  end
+
+  run!
 end
